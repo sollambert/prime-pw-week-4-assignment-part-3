9c17a7d48ae201c8043378c41fb9b112b28fddd4
Solomon Lambert <sol.lambert@gmail.com>
2022-12-19 15:14:41 -0600

    Create addItem function and global basket variable, use function and log output

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,17 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+function addItem( item ) {
+    let basketLength = basket.length;
+    basket.push(item);
+    if (basketLength < basket.length) {
+        return true;
+    }
+    return false;
+}
+let item1 = "bananas"
+console.log(`Adding ${item1} to basket:`, addItem(item1));
+console.log(`New basket contents: ${basket}`)
